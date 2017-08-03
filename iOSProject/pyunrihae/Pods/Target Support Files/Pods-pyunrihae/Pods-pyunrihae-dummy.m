2d9dddd82e1670690f6b0aaa176c016abe5ab2e2
JuneBuug <junee613@gmail.com>
2017-08-03 15:17:19 +0900

    170803- Pods 폴더 untracked로 만듦

--- iOSProject/pyunrihae/Pods/Target Support Files/Pods-pyunrihae/Pods-pyunrihae-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pyunrihae : NSObject
-@end
-@implementation PodsDummy_Pods_pyunrihae
-@end
--- iOSProject/pyunrihae/Pods/Target Support Files/Pods-pyunrihaeTests/Pods-pyunrihaeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pyunrihaeTests : NSObject
-@end
-@implementation PodsDummy_Pods_pyunrihaeTests
-@end
