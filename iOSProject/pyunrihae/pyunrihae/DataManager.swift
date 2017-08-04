d456c7d09b3a37b009093cc745d9ff9bb444c45d
JuneBuug <junee613@gmail.com>
2017-08-04 13:38:31 +0900

    170804 편의점 별로 이벤트 정보 받아오는 함수 추가

--- iOSProject/pyunrihae/pyunrihae/DataManager.swift
+++ iOSProject/pyunrihae/pyunrihae/DataManager.swift
@@ -16,7 +16,7 @@ class DataManager{
     static var ref : DatabaseReference! = Database.database().reference()
     
     
-    // id순서로 이벤트 정보 배열을 받아오기.
+    // id 순서 이벤트 리스트 리턴
     static func getEventInfoOrdeyById() -> [Event]{
         
         var eventList : [Event] = []
@@ -34,6 +34,28 @@ class DataManager{
             }
         })
         
+        return eventList
+    }
+    
+    // 원하는 편의점 별 이벤트 리스트 리턴
+    static func getEventInfoByBrand(store_name: String) -> [Event]{
+        
+        var eventList : [Event] = []
+        
+        let localRef = ref.child("event")
+        let query  = localRef.queryOrdered(byChild: "brand").queryEqual(toValue: store_name)
+        query.observe(DataEventType.value, with: { (snapshot) in
+            var event : Event
+            for childSnapshot in snapshot.children {
+                event = Event.init(snapshot: childSnapshot as! DataSnapshot)
+                // ID 하위에 있는 snapshot으로 event 초기화
+                let snap = childSnapshot as? DataSnapshot
+                event.id = snap?.key ?? ""
+                //
+                eventList.append(event)
+            }
+
+        })
         return eventList
     }
 }
