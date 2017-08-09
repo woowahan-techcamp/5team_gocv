25587c24f253e719326bdf7b840531a6caa05892
JuneBuug <junee613@gmail.com>
2017-08-09 11:48:27 +0900

    170809 - Review 데이터 업데이트에 따라 받아오는 모델 수정

--- iOSProject/pyunrihae/pyunrihae/Review.swift
+++ iOSProject/pyunrihae/pyunrihae/Review.swift
@@ -13,9 +13,12 @@ import FirebaseDatabase
 class Review {
     
     var id : String  // 리뷰 아이디
-    var image : String // 사용자가 올린 리뷰 사진
     var user : String // 사용자 닉네임
-    var product_key : String // 리뷰가 달린 상품의 key
+    var p_image : String // 사용자가 올린 리뷰 사진
+    var p_price : Int // 리뷰한 상품 가격
+    var p_id : String // 리뷰가 달린 상품의 key
+    var p_name : String // 리뷰한 상품 이름
+    var commmet : String // 리뷰 내용
     var timestamp : String // timestamp
     var brand : String // 리뷰한 상품의 브랜드
     var category : String // 리뷰한 상품의 카테고리
@@ -24,13 +27,14 @@ class Review {
     var flavor : Int // 맛에 대해서 리뷰어가 준 점수 (1~5)
     var quantity : Int // 양에 대해서 리뷰어가 준 점수 (1~5)
     var useful : Int // 이 리뷰가 유용하다고 평가한 사람 수
-    var bad : Int // 이 리뷰가 별로라고 평가한 사람 수 
+    var bad : Int // 이 리뷰가 별로라고 평가한 사람 수
     
     init() {
         self.id = ""
-        self.image = ""
+        self.p_image = ""
         self.user = ""
-        self.product_key = ""
+        self.p_id = ""
+        self.p_price = 0
         self.timestamp  = ""
         self.brand = ""
         self.category = ""
@@ -40,14 +44,17 @@ class Review {
         self.quantity = 0
         self.useful = 0
         self.bad = 0
+        self.p_name = ""
+        self.commmet = ""
     }
     
     
-    init(id : String, image : String, user : String, product_key : String, timestamp : String, brand : String, category : String, grade : Int, price : Int, flavor : Int, quantity : Int, useful : Int, bad :Int ){
+    init(id : String, p_image : String, user : String, p_id : String, p_price : Int,timestamp : String, brand : String, category : String, grade : Int, price : Int, flavor : Int, quantity : Int, useful : Int, bad :Int , p_name : String, comment: String){
         self.id = id
-        self.image = image
+        self.p_image = p_image
         self.user = user
-        self.product_key = product_key
+        self.p_id = p_id
+        self.p_price = p_price
         self.timestamp = timestamp
         self.brand = brand
         self.category = category
@@ -57,14 +64,17 @@ class Review {
         self.quantity = quantity
         self.useful = useful
         self.bad = bad
+        self.p_name = p_name
+        self.commmet = comment
     }
     
     convenience init(snapshot : DataSnapshot){
         let dict = snapshot.value as? Dictionary<String,AnyObject> ?? [:]
         let id = dict["id"] as? String ?? ""
-        let image = dict["image"] as? String ?? ""
+        let p_image = dict["p_image"] as? String ?? ""
         let user = dict["user"] as? String ?? ""
-        let product_key = dict["product_key"] as? String ?? ""
+        let p_id = dict["p_id"] as? String ?? ""
+        let p_price = dict["p_price"] as? Int ?? 0
         let timestamp = dict["timestamp"] as? String ?? ""
         let brand = dict["brand"] as? String ?? ""
         let category = dict["category"] as? String ?? ""
@@ -74,7 +84,9 @@ class Review {
         let quantity = dict["quantity"] as? Int ?? 0
         let useful = dict["useful"] as? Int ?? 0
         let bad = dict["bad"] as? Int ?? 0
+        let p_name = dict["p_name"] as? String ?? ""
+        let comment = dict["comment"] as? String ?? ""
         
-        self.init(id: id,image : image, user: user, product_key: product_key, timestamp : timestamp, brand: brand, category: category, grade: grade, price : price, flavor: flavor, quantity : quantity, useful: useful, bad : bad)
+        self.init(id: id,p_image : p_image, user: user, p_id: p_id, p_price: p_price, timestamp : timestamp, brand: brand, category: category, grade: grade, price : price, flavor: flavor, quantity : quantity, useful: useful, bad : bad, p_name: p_name, comment: comment)
     }
 }
