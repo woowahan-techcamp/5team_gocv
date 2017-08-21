a9ff9fd544d28ae965f63e20182a39c0076770cd
wooooooong <hjw01234@gmail.com>
2017-08-21 20:41:29 +0900

    170821 #146 cell 크기 변경

--- iOSProject/pyunrihae/pyunrihae/Label.swift
+++ iOSProject/pyunrihae/pyunrihae/Label.swift
@@ -16,4 +16,14 @@ class Label{
         label.layer.cornerRadius = label.layer.frame.height/2
         label.clipsToBounds = true
     }
+    static func heightForView(text:String, font:UIFont, width:CGFloat) -> CGFloat{
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = font
+        label.text = text
+        label.sizeToFit()
+        
+        return label.frame.height
+    }
 }
