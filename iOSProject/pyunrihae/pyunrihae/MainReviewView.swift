899db535a4a8a43ee03ab5b8dd84cb09a45dae2a
JuneBuug <junee613@gmail.com>
2017-08-22 13:35:25 +0900

    20170822 - Main Review View 제작 #143

---
+++ iOSProject/pyunrihae/pyunrihae/MainReviewView.swift
@@ -0,0 +1,21 @@
+//
+//  MainReviewView.swift
+//  pyunrihae
+//
+//  Created by woowabrothers on 2017. 8. 22..
+//  Copyright © 2017년 busride. All rights reserved.
+//
+
+import UIKit
+
+class MainReviewView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
